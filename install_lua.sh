dfcfa4ae9154a7d26d1dfed139b64ed8826e01c7
cyril0124 <211998078@qq.com>
2023-10-29 23:26:46 +0800

    Change luajit to openresty's luajit

--- install_lua.sh
+++ install_lua.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
+curr_dir=$(pwd)
 luajit_dir=$(pwd)/luajit2.1
+luajit_branch=v2.1-20231006
 
 rm -rf $luajit_dir
-hererocks luajit2.1 -j 2.1 -r latest
+git clone https://github.com/openresty/luajit2.git $luajit_dir
+cd $luajit_dir
+git checkout $luajit_branch
+cd $curr_dir
+
+hererocks luajit2.1 -j $luajit_dir -r latest
 
 # Bug fix...
-cp $luajit_dir/lib/libluajit-5.1.so.2 $luajit_dir/lib/libluajit-5.1.so
+# cp $luajit_dir/lib/libluajit-5.1.so.2 $luajit_dir/lib/libluajit-5.1.so
