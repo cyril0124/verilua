b4197c01d9f703108f33551008da08a7e3e6f35b
cyril0124 <211998078@qq.com>
2023-11-12 17:21:49 +0800

    Update scripts/rt_plot/jobs.sh

--- scripts/rt_plot/jobs.sh
+++ scripts/rt_plot/jobs.sh
@@ -10,3 +10,6 @@ python3 rt_plot.py -p 12356 -t hit_slice_1_a -x time -y hit_rate -i 10 > hit_sli
 
 # python3 rt_plot.py -p 12365 -t hit_slice_0_c -x time -y hit_rate -i 10 > hit_slice_0_c.out 2>&1
 # python3 rt_plot.py -p 12366 -t hit_slice_1_c -x time -y hit_rate -i 10 > hit_slice_1_c.out 2>&1
+
+python3 rt_plot.py -p 12365 -t hit_slice_0_total_a -x time -y hit_rate -i 10 > hit_slice_0_total_a.out 2>&1
+python3 rt_plot.py -p 12366 -t hit_slice_1_total_a -x time -y hit_rate -i 10 > hit_slice_1_total_a.out 2>&1
