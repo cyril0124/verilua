dd1b107ffee7acc4cc9dae93c0dd81d19442cb58
cyril0124 <211998078@qq.com>
2024-04-07 01:44:19 +0800

    Add CyclicRandom.lua

---
+++ src/lua/verilua/CyclicRandom.lua
@@ -0,0 +1,45 @@
+local class = require "pl.class"
+local assert = assert
+local random, randomseed = math.random, math.randomseed
+
+local CyclicdRandom = class()
+
+function CyclicdRandom:_init(start_value, end_value)
+    assert(end_value > start_value)
+
+    self.gen_idx = 1
+    self.size = end_value - start_value + 1
+    self.values = {}
+
+    local idx = 1
+    for i = start_value, end_value do
+        self.values[idx] = i
+        idx = idx + 1
+    end
+
+    self:shuffle(0)
+end
+
+function CyclicdRandom:shuffle(seed)
+    randomseed(seed)
+    local n = #self.values
+    while n >= 2 do
+      local k = random(n)
+      self.values[n], self.values[k] = self.values[k], self.values[n]
+      n = n - 1
+    end
+end
+
+function CyclicdRandom:gen(seed)
+    if self.gen_idx > self.size then
+        local _seed = seed or 0
+        self:shuffle(_seed)
+        self.gen_idx = 1
+    end
+    
+    local value = self.values[self.gen_idx]
+    self.gen_idx = self.gen_idx + 1
+    return value
+end
+
+return CyclicdRandom
