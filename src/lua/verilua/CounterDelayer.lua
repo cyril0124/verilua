9f16d01acfc1943bbffab2e1d5f303e32e6eee37
cyril0124 <211998078@qq.com>
2024-09-06 15:05:15 +0800

    feat: CounterDelayer will check for validity of min/max_delay value

--- src/lua/verilua/CounterDelayer.lua
+++ src/lua/verilua/CounterDelayer.lua
@@ -5,6 +5,8 @@ local random, f = math.random, string.format
 local CounterDelayer = class()
 
 function CounterDelayer:_init(min_delay, max_delay)
+    assert(min_delay ~= nil)
+    assert(max_delay ~= nil)
     assert(max_delay >= min_delay, f("max_delay:%d min_delay:%d", max_delay, min_delay))
 
     self.min_delay = min_delay
