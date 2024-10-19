9a70fab22ef8e071d583200dcf0c460e324647e3
cyril0124 <211998078@qq.com>
2024-10-19 17:08:49 +0800

    chore: upgrate `verilator-clang.nix`

--- nix/verilator-clang.nix
+++ nix/verilator-clang.nix
@@ -1,5 +1,6 @@
-{ pkgs ? import <nixpkgs> {} }:
-pkgs.verilator.overrideAttrs (final: prev: {
+{ pkgs ? import <nixpkgs> {}, 
+  pkgsu ? import (builtins.fetchTarball "https://github.com/NixOS/nixpkgs/archive/4a793e2f3288b8f89430aab927d08d347e20b83e.tar.gz") {} 
+}: pkgsu.verilator.overrideAttrs (final: prev: {
   name = "verilator-clang";
   configureFlags = [
     "CC=${pkgs.clang_18}/bin/clang"
