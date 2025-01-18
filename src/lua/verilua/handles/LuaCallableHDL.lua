4c9ca460022c3c24bc289e9f27a80463f9a87bbe
cyril0124 <211998078@qq.com>
2025-01-18 21:46:40 +0800

    chore(CallableHDL): remove `get_width()`

--- src/lua/verilua/handles/LuaCallableHDL.lua
+++ src/lua/verilua/handles/LuaCallableHDL.lua
@@ -838,10 +838,6 @@ function CallableHDL:_init(fullpath, name, hdl)
         print(this:dump_str())
     end
 
-    self.get_width = function (this)
-        return this.width
-    end
-
     self.expect = function (this, value)
         local typ = type(value)
         assert(typ == "number" or typ == "cdata")
