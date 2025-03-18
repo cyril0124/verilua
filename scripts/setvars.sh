ab117f4327d92842ad5efa4cdfa39a679bc136f0
cyril0124 <211998078@qq.com>
2025-03-18 11:36:42 +0800

    chore: remove unused

--- scripts/setvars.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-source $VERILUA_HOME/activate_verilua.sh
-
-# !! You should modify these two variables according to your project.
-export VERILUA_CFG="verilua_cfg_unknown"
-export SIM="verilator"
