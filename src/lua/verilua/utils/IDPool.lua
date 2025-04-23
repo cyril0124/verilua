18e4c551a328077119b63bc24fe116c6b6b8e34f
cyril0124 <211998078@qq.com>
2025-04-23 23:12:25 +0800

    refactor(IDPool): add type annotations

--- src/lua/verilua/utils/IDPool.lua
+++ src/lua/verilua/utils/IDPool.lua
@@ -6,6 +6,25 @@ local type = type
 local assert = assert
 local f = string.format
 
+---@class IDPool.params
+---@field size number
+---@field start_value? number
+---@field shuffle? boolean
+
+---@class IDPool
+---@overload fun(pool_size_or_params: number|IDPool.params, shuffle?: boolean): IDPool
+---@field pool_size number
+---@field start_value number
+---@field end_value number
+---@field pool table<number>
+---@field size number
+---@field alloc fun(self: IDPool): number
+---@field release fun(self: IDPool, id: number)
+---@field is_full fun(self: IDPool): boolean Check if the pool is full (all IDs are available)
+---@field is_empty fun(self: IDPool): boolean Check if the pool is empty(all IDs are allocated, none left in pool)
+---@field used_count fun(self: IDPool): number
+---@field free_count fun(self: IDPool): number
+---@operator len: number
 local IDPool = class()
 
 -- 
@@ -18,9 +37,16 @@ local IDPool = class()
 --      local id = idpool:alloc()
 --      idpool:release(id)
 -- 
+
+---@param params number|IDPool.params
+---@param shuffle boolean?
 function IDPool:_init(params, shuffle)
+    ---@cast self IDPool
+    local self = self
+
     local _shuffle = false
     if type(params) == "table" then
+        ---@cast params IDPool.params
         _shuffle = params.shuffle or false
 
         self.pool_size = assert(params.size, "[IDPool] size is required")
@@ -28,6 +54,7 @@ function IDPool:_init(params, shuffle)
         self.start_value = params.start_value or 0
         self.end_value = self.start_value + self.pool_size - 1
     else
+        ---@cast params number
         self.start_value = 0
         self.end_value = params - 1
         self.pool_size = params
