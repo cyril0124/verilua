fc144b7723889f77a87f6afe5f1f37cc3433a20b
cyril0124 <211998078@qq.com>
2025-05-06 00:30:44 +0800

    feat(vpiml): implement handle caching

--- src/lua/verilua/vpiml/vpiml.lua
+++ src/lua/verilua/vpiml/vpiml.lua
@@ -1,5 +1,40 @@
+local vpiml
+local handle_cache = {}
+
 if os.getenv("VL_PREBUILD") == "1" then
-    return require "VpimlPrebuild"
+    vpiml = require "VpimlPrebuild"
 else
-    return require "VpimlNormal"
+    vpiml = require "VpimlNormal"
 end
+
+local vpiml_handle_by_name = vpiml.vpiml_handle_by_name
+local vpiml_handle_by_name_safe = vpiml.vpiml_handle_by_name_safe
+
+---@diagnostic disable-next-line: duplicate-set-field
+vpiml.vpiml_handle_by_name = function (name)
+    local handle = handle_cache[name]
+    if handle then
+        return handle
+    end
+
+    handle = vpiml_handle_by_name(name)
+    handle_cache[name] = handle
+    return handle
+end
+
+---@diagnostic disable-next-line: duplicate-set-field
+vpiml.vpiml_handle_by_name_safe = function (name)
+    local handle = handle_cache[name]
+    if handle then
+        assert(handle ~= -1)
+        return handle
+    end
+
+    handle = vpiml_handle_by_name_safe(name)
+    if handle ~= -1 then
+        handle_cache[name] = handle
+    end
+    return handle
+end
+
+return vpiml
