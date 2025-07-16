a1710709153680843c565bff0d323c1d0c172d6e
cyril0124 <211998078@qq.com>
2025-07-16 22:47:48 +0800

    build(libverilua): improve library search path handling

--- libverilua/build.rs
+++ libverilua/build.rs
@@ -1,11 +1,15 @@
 fn main() {
     if std::env::var("CARGO_FEATURE_IVERILOG_VPI_MOD").is_ok() {
+        println!("cargo:rustc-link-lib=static=vpi");
+        println!("cargo:rustc-link-lib=static=veriuser");
+
         if let Ok(iverilog_home) = std::env::var("IVERILOG_HOME") {
-            println!("cargo:rustc-link-lib=static=vpi");
-            println!("cargo:rustc-link-lib=static=veriuser");
             println!("cargo:rustc-link-search=native={iverilog_home}/lib");
-        } else {
-            panic!("IVERILOG_HOME is not set");
+        }
+        if let Ok(paths) = std::env::var("LD_LIBRARY_PATH") {
+            for path in paths.split(':') {
+                println!("cargo:rustc-link-search=native={path}");
+            }
         }
     }
 
