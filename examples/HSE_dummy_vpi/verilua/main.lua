8d9a37443549950fd9055165728abdb4784dad59
cyril0124 <211998078@qq.com>
2025-08-07 21:31:42 +0800

    feat(examples/HSE_dummy_vpi): add assertions for expected values

--- examples/HSE_dummy_vpi/verilua/main.lua
+++ examples/HSE_dummy_vpi/verilua/main.lua
@@ -7,12 +7,16 @@ fork {
         local lua_hexStr2 = ""
         local lua_hexStr3 = ""
 
-        dut.clk:posedge(10, function (i)
+        local expected_values = { 0, 0, 0, 1, 2, 3, 4, 5, 6, 7 }
+        dut.clk:posedge(10, function(i)
             local v = value:get()
             lua_hexStr = value:get_hex_str()
             lua_hexStr2 = value2:get_hex_str()
             lua_hexStr3 = value3:get_hex_str()
             print("[" .. i .. "]", v, lua_hexStr, lua_hexStr2, lua_hexStr3)
+
+            assert(v == expected_values[i])
+            assert(value2:get() == expected_values[i] * 3)
         end)
 
         sim.finish()
