f826a167be5ee7d6f29ac5eebdb162a4bde31af5
cyril0124 <211998078@qq.com>
2025-11-21 18:16:26 +0800

    feat(libverilua): increase edge callback ID pool size from 10k to 100k

--- libverilua/src/gen/gen.lua
+++ libverilua/src/gen/gen.lua
@@ -403,7 +403,7 @@ Self {
     #[cfg(not(feature = "chunk_task"))]
     pending_edge_cb_map: HashMap::new(),
 
-    edge_cb_idpool: IDPool::new(10000),
+    edge_cb_idpool: IDPool::new(100000), // The maximum number of edge callbacks is controlled by this value
     edge_cb_hdl_map: HashMap::new(),
 
     resolve_x_as_zero: false,
