c741b8afb1cd1339904f98ef933037bdc6d8e21d
cyril0124 <211998078@qq.com>
2026-01-05 20:57:28 +0800

    refactor(IDPool): refine implementation

--- src/lua/verilua/utils/IDPool.lua
+++ src/lua/verilua/utils/IDPool.lua
@@ -3,6 +3,7 @@
 local class = require "pl.class"
 local utils = require "LuaUtils"
 local table_new = require "table.new"
+local table_clear = require "table.clear"
 
 local type = type
 local assert = assert
@@ -16,12 +17,13 @@ local f = string.format
 ---@class (exact) verilua.utils.IDPool
 ---@overload fun(params: verilua.utils.IDPool.params): verilua.utils.IDPool
 ---@overload fun(pool_size_or_params: integer, shuffle?: boolean): verilua.utils.IDPool
----@field private pool_size integer
----@field private start_value integer
----@field private end_value integer
+---@field private POOL_SIZE integer
+---@field private START_VALUE integer
+---@field private END_VALUE integer
 ---@field private shuffle boolean
 ---@field private pool integer[]
 ---@field private size integer
+---@field private allocated table<integer, boolean> -- Track allocated IDs for O(1) duplicate check
 ----@field alloc fun(self: verilua.utils.IDPool): integer
 ---@field release fun(self: verilua.utils.IDPool, id: integer)
 ---@field is_full fun(self: verilua.utils.IDPool): boolean Check if the pool is full (all IDs are available)
@@ -54,23 +56,24 @@ function IDPool:_init(params, shuffle)
         ---@cast params verilua.utils.IDPool.params
         _shuffle = params.shuffle or false
 
-        self.pool_size = assert(params.size, "[IDPool] size is required")
+        self.POOL_SIZE = assert(params.size, "[IDPool] size is required")
 
-        self.start_value = params.start_value or 0
-        self.end_value = self.start_value + self.pool_size - 1
+        self.START_VALUE = params.start_value or 0
+        self.END_VALUE = self.START_VALUE + self.POOL_SIZE - 1
     else
         ---@cast params integer
-        self.start_value = 0
-        self.end_value = params - 1
-        self.pool_size = params
+        self.START_VALUE = 0
+        self.END_VALUE = params - 1
+        self.POOL_SIZE = params
         _shuffle = shuffle or false
     end
 
-    self.pool = table_new(self.pool_size, 0)
-    self.size = self.pool_size
+    self.pool = table_new(self.POOL_SIZE, 0)
+    self.size = self.POOL_SIZE
+    self.allocated = {} -- Track allocated IDs for O(1) duplicate check
 
     local index = 1
-    for i = self.end_value, self.start_value, -1 do
+    for i = self.END_VALUE, self.START_VALUE, -1 do
         self.pool[index] = i
         index = index + 1
     end
@@ -84,48 +87,54 @@ end
 ---@nodiscard Return value(ID) should be used
 ---@return integer
 function IDPool:alloc()
-    local id = self.pool[self.size]
-    self.pool[self.size] = nil
-    self.size = self.size - 1
+    local size = self.size
+    local id = self.pool[size]
+    self.pool[size] = nil
+    self.size = size - 1
+    self.allocated[id] = true
     assert(self.size >= 0, "[IDPool] pool is empty")
     return id --[[@as integer]]
 end
 
 function IDPool:release(id)
-    if id < self.start_value or id > self.end_value then
+    if id < self.START_VALUE or id > self.END_VALUE then
         assert(
             false,
             f(
-                "[IDPool] id is out of range: %d, start_value => %d, end_value => %d",
+                "[IDPool] id is out of range: %d, START_VALUE => %d, END_VALUE => %d",
                 id,
-                self.start_value,
-                self.end_value
+                self.START_VALUE,
+                self.END_VALUE
             )
         )
     end
 
-    for i = 1, self.size do
-        if self.pool[i] == id then
-            assert(false, f("[IDPool] id is already in the pool: %d", id))
-        end
+    if not self.allocated[id] then
+        assert(
+            false,
+            f("[IDPool] id is not allocated or already in the pool: %d", id)
+        )
     end
 
     self.size = self.size + 1
     self.pool[self.size] = id
+    self.allocated[id] = nil -- Remove from allocated set
 end
 
 -- Check if the pool is full (all IDs are available)
 function IDPool:is_full()
-    return self.size == 0
+    assert(false, "<IDPool>:is_full() is deprecated, use `<IDPool>:used_count() == 0` instead")
+    return self.size == self.POOL_SIZE
 end
 
 -- Check if the pool is empty(all IDs are allocated, none left in pool)
 function IDPool:is_empty()
-    return self.size == self.pool_size
+    assert(false, "<IDPool>:is_empty() is deprecated, use `<IDPool>:free_count() == 0` instead")
+    return self.size == 0
 end
 
 function IDPool:used_count()
-    return self.pool_size - self.size
+    return self.POOL_SIZE - self.size
 end
 
 function IDPool:free_count()
@@ -133,11 +142,12 @@ function IDPool:free_count()
 end
 
 function IDPool:reset()
-    self.size = self.pool_size
-    self.pool = table_new(self.pool_size, 0)
+    self.size = self.POOL_SIZE
+    self.pool = table_new(self.POOL_SIZE, 0)
+    table_clear(self.allocated)
 
     local index = 1
-    for i = self.end_value, self.start_value, -1 do
+    for i = self.END_VALUE, self.START_VALUE, -1 do
         self.pool[index] = i
         index = index + 1
     end
@@ -148,8 +158,7 @@ function IDPool:reset()
 end
 
 function IDPool:__len()
-    -- equivalent to self.pool_size
-    return #self.pool
+    return self.POOL_SIZE
 end
 
 return IDPool
