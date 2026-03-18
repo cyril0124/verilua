92d3c1d691d32cc9824da137c8e948731a391ec7
xieby1 <xieby1@outlook.com>
2026-03-18 16:35:24 +0800

    feat(nix): add luajit-pro support

---
+++ scripts/nix/luajit-pro/default.nix
@@ -0,0 +1,20 @@
+# TODO: the git submodule luajit-pro is redundant, can be removed
+let
+  npinsed = import ../../../npins;
+  pkgs = import npinsed.nixpkgs {};
+  luajit-pro = (pkgs.luajit_openresty.override {
+    self = luajit-pro;
+    version = "pro";
+    src = npinsed.luajit2;
+  }).overrideAttrs (old: {
+    postPatch = ''
+      cp -f ${npinsed.luajit-pro}/patch/src/* src/
+      # Nix can handle the ldflags, so remove the patched one
+      sed -i 's,-Wl.*target/release),,' src/Makefile
+    '' + old.postPatch;
+    buildFlags = [];
+    buildInputs = old.buildInputs ++ [
+      (import ./helper.nix)
+    ];
+  });
+in luajit-pro
