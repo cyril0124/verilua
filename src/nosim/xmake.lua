3f0d355ace802e121bf84163bb9a347e6af47dae
cyril0124 <211998078@qq.com>
2026-08-22 11:25:53 +0800

    fix: link fmt when building nosim
    Dropping libassert also dropped the conan libdir and -lfmt, so vl_assert failed to link.

--- src/nosim/xmake.lua
+++ src/nosim/xmake.lua
@@ -37,6 +37,9 @@ target("nosim", function()
         "-Wl,--no-as-needed"
     )
 
+    add_links("fmt")
+    add_linkdirs(path.join(libs_dir, "lib"))
+    add_rpathdirs(path.join(libs_dir, "lib"))
 
     add_linkgroups("luajit-5.1", { static = true, whole = true })
     add_linkdirs(path.join(lua_dir, "lib"))
